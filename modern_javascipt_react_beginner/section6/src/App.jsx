78a64cc9c823072f0886c8bddfa69de46057593c
dkntksh <mr.kaz71@gmail.com>
2025-10-20 22:08:39 +0900

    記載方法のメモ追加

--- modern_javascipt_react_beginner/section6/src/App.jsx
+++ modern_javascipt_react_beginner/section6/src/App.jsx
@@ -1,8 +1,18 @@
 export const App = () => {
+  const onClickButton = () => alert();
+  const contentStyle = {
+    color: "blue",// 文字列で定義する
+    fontSize: "45px",  // font-sizeがfontSizeというキャメルケースの記載になる
+    margin: 100, // 数字での記載も可能
+  };
   return (
     <>
-      <h1>こんにちは!</h1>
+      // インラインでスタイルを当てることも可能
+      <h1 style={contentStyle}>こんにちは!</h1>
       <p>おげんきですか？</p>
+      // キャメルケースでイベントを定義：onclick→onClickにする
+      // {}の中は関数を指定できる
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };

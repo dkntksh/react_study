a6ad571aa5b2f89d39cb62eb4ff45a687318ecee
dkntksh <mr.kaz71@gmail.com>
2025-10-21 20:36:02 +0900

    再レンダリングとuseEffect

--- modern_javascipt_react_beginner/section6/src/App.jsx
+++ modern_javascipt_react_beginner/section6/src/App.jsx
@@ -1,23 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ColorfulMessage } from "./components/ColorfulMessage";
 
 export const App = () => {
+  console.log("-- APP --");
   // numの初期値を()で指定し、更新関数を,の後で指定
   // 一番上の構造からしか、userXXXのようなフックは呼び出せない
   const [num, setNum] = useState(0);
+  const [isShowFace, setIsShowFace] = useState(false);
   const onClickCountUp = () => {
     // numの更新関数を設定する
     // バッチ化されており、まとめて処理するようになっている
     setNum((prev) => prev + 1);
-    setNum((prev) => prev + 1);
-    // setNum(num + 1);
   };
+  const onClickToggle = () => {
+    setIsShowFace(!isShowFace);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        isShowFace || setIsShowFace(true);
+      } else {
+        isShowFace && setIsShowFace(false);
+      }
+    }
+  }, [num]);
+
   const contentStyle = {
     color: "blue",
-    fontSize: "45px",
-  };
-  const contentStyleB = {
-    color: "green",
     fontSize: "45px",
   };
   return (
@@ -27,6 +37,8 @@ export const App = () => {
       <ColorfulMessage color="green">元気です!</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
       <p>{num}</p>
+      <button onClick={onClickToggle}>on/off</button>
+      {isShowFace && <p>( ;∀;)</p>}
     </>
   );
 };
